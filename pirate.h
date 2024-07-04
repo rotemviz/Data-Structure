73833602e8c69606674a349707d5bb1fae0f9009
Adir <adirlevi887@gmail.com>
2024-07-04 22:24:41 +0300

    Pirate

---
+++ pirate.h
@@ -0,0 +1,30 @@
+#ifndef EX1_Pirate_H
+#define EX1_Pirate_H
+
+#include "linkedList.h"
+
+class Pirate{
+    int m_id;
+    int m_coin;
+    Node<Pirate>* m_ptrShip;
+
+public:
+    Pirate(int id, int coin) : m_id(id),m_coin(coin),m_ptrShip(nullptr){}
+    void setPtrShip(Node<Pirate>* ptr){
+        m_ptrShip = ptr;
+    }
+    bool setCoin(int change){
+        m_coin += change;
+    }
+    int getCoin()const{
+        return m_coin;
+    }
+    int getId()const{
+        return m_id;
+    }
+    Node<Pirate>* getPtrShip()const{
+        return m_ptrShip;
+    }
+};
+
+#endif //EX3_Pirate_H
--- pirates.h
+++
@@ -1,5 +0,0 @@
-#ifndef EX1_Pirates_H
-#define EX1_Pirates_H
-
-
-#endif //EX3_Pirates_H
